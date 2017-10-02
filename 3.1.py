6161ee4d3e86962e1e5edcecec3463b89c092feb
Bradleyt91 <32064739+Bradleyt91@users.noreply.github.com>
2017-10-02 14:12:15 +0100

    Add files via upload

---
+++ 3.1.py
@@ -0,0 +1,11 @@
+myNum1 = int(input("Please type in the first NUMBER"))
+
+myNum2 = int(input("Please type second Number"))
+
+mySum = myNum1 + myNum2
+                   
+print(mySum)
+                   
+                   
+
+                
