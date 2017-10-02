23f588c43f073d1e025252e55052cb513f3d65b7
Bradleyt91 <32064739+Bradleyt91@users.noreply.github.com>
2017-10-02 14:23:15 +0100

    Update 3.1.py
    remove the codes for the second number

--- 3.1.py
+++ 3.1.py
@@ -1,8 +1,4 @@
 myNum1 = int(input("Please type in the first NUMBER"))
-
-myNum2 = int(input("Please type second Number"))
-
-mySum = myNum1 + myNum2
                    
 print(mySum)
                    
