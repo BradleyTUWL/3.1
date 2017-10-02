9e7499ccfcab6ac467bf6f71bda72c8261af6705
Bradleyt91 <32064739+Bradleyt91@users.noreply.github.com>
2017-10-02 14:30:11 +0100

    Update 3.1.py
    changed print(myNum) to print(mysum)

--- 3.1.py
+++ 3.1.py
@@ -1,7 +1,7 @@
 myNum1 = int(input("Please type in the first NUMBER"))      
 myNum2 = int(input("Please type in the second NUMBER"))
 myNum3 = int(input("please type in the third NUMBER"))
-print(myNum1)
+print(mysum)
                    
                    
 
