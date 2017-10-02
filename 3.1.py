a8a2570b2fd2d09e38f28817c1fe23e261020621
Bradleyt91 <32064739+Bradleyt91@users.noreply.github.com>
2017-10-02 14:32:00 +0100

    Update 4.2
    Added formula to line 4 for calculations

--- 3.1.py
+++ 3.1.py
@@ -1,7 +1,8 @@
 myNum1 = int(input("Please type in the first NUMBER"))      
 myNum2 = int(input("Please type in the second NUMBER"))
 myNum3 = int(input("please type in the third NUMBER"))
-print(mysum)
+mySum = myNum1 + myNum2 + myNum3
+print(mySum)
                    
                    
 
